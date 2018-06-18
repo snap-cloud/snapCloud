20eb33f2ef49fdcee9da8ad31cbc97ea56a79cfc
Michael Ball <cycomachead@gmail.com>
2018-06-18 02:01:03 -0700

    A working way of including configs by environment

--- config.lua
+++ config.lua
@@ -68,3 +68,15 @@ config('production', {
     -- TODO: See if we can turn this on without a big hit
     measure_performance = false
 })
+
+-- Compile nginx helper files.
+local path = require('lapis.cmd.path')
+local env = require("lapis.cmd.util").default_environment()
+local additional_config = 'nginx.conf.d/' .. env .. '.conf'
+local template = path.read_file(additional_config)
+local ConfigCompiler = require("lapis.cmd.nginx.config").ConfigCompiler
+local complete_config = config.get()
+local output = ConfigCompiler:compile_config(
+    template, complete_config, { header = false }
+)
+path.write_file(additional_config .. '.compiled', output)
