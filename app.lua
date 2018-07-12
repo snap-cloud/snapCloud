1bda04a8b74ef3f740efb65a587da3191d04bf15
Michael Ball <cycomachead@gmail.com>
2018-07-12 02:40:40 -0700

    Add a Secure attribute to cookies.
    This ensures that cookies only work when the cloud is accessed over SSL.

--- app.lua
+++ app.lua
@@ -49,7 +49,7 @@ require 'responses'
 app.cookie_attributes = function(self)
     local date = require("date")
     local expires = date(true):adddays(365):fmt("${http}")
-    return "Expires=" .. expires .. "; Path=/; HttpOnly"
+    return "Expires=" .. expires .. "; Path=/; HttpOnly; Secure"
 end
 
 -- Store whitelisted domains
