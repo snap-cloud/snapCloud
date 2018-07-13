14f8d030eac323a320a655980d4cc1a032e2fe8d
Michael Ball <cycomachead@gmail.com>
2018-07-13 00:21:06 -0700

    add CORS domains as a separate file

--- app.lua
+++ app.lua
@@ -43,6 +43,9 @@ package.loaded.config = require("lapis.config").get()
 
 local app = package.loaded.app
 
+-- Store whitelisted domains
+local domain_allowed = require('cors')
+
 require 'responses'
 
 -- Make cookies persistent
@@ -51,42 +54,6 @@ app.cookie_attributes = function(self)
     local expires = date(true):adddays(365):fmt("${http}")
     return "Expires=" .. expires .. "; Path=/; HttpOnly; Secure"
 end
-
--- Store whitelisted domains
-local domain_allowed = {}
-domain_allowed['snap.berkeley.edu'] = true
-domain_allowed['snap-cloud.cs10.org'] = true
-domain_allowed['cloud.snap.berkeley.edu'] = true
-domain_allowed['amazingrobots.net'] = true
-domain_allowed['snap4arduino.rocks'] = true
--- Snap4Arduino for Chromebooks
-domain_allowed['chrome-extension://bdmapaboflkhdmcgdpfooeeeadejodia'] = true
--- Snap! Mirrors
-domain_allowed['cs10.org'] = true
-domain_allowed['bjc.edc.org'] = true
-domain_allowed['byob.eecs.berkeley.edu'] = true
-domain_allowed['web.media.mit.edu'] = true
-domain_allowed['snap.apps.miosoft.com'] = true
--- Snap! Research Projects
-domain_allowed['eliza.csc.ncsu.edu'] = true
-domain_allowed['arena.csc.ncsu.edu'] = true
-domain_allowed['stemc.csc.ncsu.edu'] = true
-domain_allowed['lambda.cs10.org'] = true
--- All edX Sites, and test sites
-domain_allowed['courses.edge.edx.org'] = true
-domain_allowed['courses.edx.org'] = true
-domain_allowed['d37djvu3ytnwxt.cloudfront.net'] = true
-domain_allowed['preview.courses.edge.edx.org'] = true
-domain_allowed['preview.courses.edx.org'] = true
-domain_allowed['preview.edge.edx.org'] = true
-domain_allowed['preview.edx.org'] = true
-domain_allowed['studio.edge.edx.org'] = true
-domain_allowed['studio.edx.org'] = true
-domain_allowed['edge.edx.org'] = true
--- Development
-domain_allowed['romagosa.work'] = true
-domain_allowed['localhost'] = true
-
 
 -- Database abstractions
 
