b31f3605550d81c5100690e42967ff61971cdb4f
Bernat Romagosa <bromagosa@e-citilab.eu>
2019-01-17 11:28:09 +0100

    added db dependency to project.lua

--- controllers/project.lua
+++ controllers/project.lua
@@ -20,6 +20,7 @@
 -- You should have received a copy of the GNU Affero General Public License
 -- along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
+local db = package.loaded.db
 local cached = package.loaded.cached
 local Projects = package.loaded.Projects
 local DeletedProjects = package.loaded.DeletedProjects
