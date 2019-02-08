87aae849d3dc894fbd6022bcdaf929fb31d6caaa
Bernat Romagosa <bromagosa@e-citilab.eu>
2019-02-08 12:04:35 +0100

    mixed up langs again...

--- controllers/collection.lua
+++ controllers/collection.lua
@@ -157,7 +157,7 @@ CollectionController = {
             assert_can_view_collection(self, collection)
             collection.projects_count = collection:count_projects()
             collection.creator =
-                Users:find(collection.creator_id, { fields: 'username, id'})
+                Users:find(collection.creator_id, { fields = 'username, id'})
             if collection.thumbnail_id then
                 collection.thumbnail =
                     disk:retrieve_thumbnail(collection.thumbnail_id)
