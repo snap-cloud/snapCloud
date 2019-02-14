bf8debef371cea6717c803cff181f00804439ab2
Bernat Romagosa <bromagosa@e-citilab.eu>
2019-02-14 11:57:41 +0100

    oops! table name is user_s_

--- controllers/project.lua
+++ controllers/project.lua
@@ -308,7 +308,7 @@ ProjectController = {
                         fields = 'collections.creator_id, collections.name, ' ..
                             'collection_memberships.project_id, '..
                             'collections.thumbnail_id, collections.shared, ' ..
-                            'collections.published, user.username',
+                            'collections.published, users.username',
                         per_page = self.params.pagesize or 16,
                         prepare_results = function (collections)
                             Users:include_in(collections, 'creator_id',
