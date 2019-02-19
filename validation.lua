71acc46c013f8963bb8e781bc7e7386bd19e6ba5
Bernat Romagosa <bromagosa@e-citilab.eu>
2019-02-19 20:08:24 +0100

    yet another error in assert_can_add_project_to_collection

--- validation.lua
+++ validation.lua
@@ -301,9 +301,11 @@ assert_can_add_project_to_collection = function (self, project, collection)
     local can_edit = collection.creator_id == self.current_user.id
 
     -- Find out whether user is in the editors array
-    for _, editor_id in pairs(collection.editor_ids) do
-        if can_edit then return end
-        can_edit = can_edit or (editor_id == self.current_user.id)
+    if collection.editor_ids then
+        for _, editor_id in pairs(collection.editor_ids) do
+            if can_edit then return end
+            can_edit = can_edit or (editor_id == self.current_user.id)
+        end
     end
 
     -- Users can add their own projects and published projects to collections
