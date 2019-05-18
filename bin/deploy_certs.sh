5c18dc0540f94f105c14e009d0d7107ff2099741
Michael Ball <cycomachead@gmail.com>
2019-05-17 22:43:57 -0700

    use full paths

--- bin/deploy_certs.sh
+++ bin/deploy_certs.sh
@@ -2,9 +2,9 @@
 
 # copy live files from config to the server
 # MUST USE -L. LetsEncrypt uses symlinks
-sourceDir=~/lets-encrypt/live/
-destDir=~/snapCloud/certs/
-source ~/snapCloud/.env
+sourceDir=/home/cloud/lets-encrypt/live/
+destDir=/home/cloud/snapCloud/certs/
+source /home/cloud/snapCloud/.env
 
 cp -r -u --verbose -L ${sourceDir}snap-${LAPIS_ENVIRONMENT}.cs10.org $destDir
 cp -r -u --verbose -L ${sourceDir}${LAPIS_ENVIRONMENT}.snap.berkeley.edu $destDir
