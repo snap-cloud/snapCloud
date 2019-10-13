f155caefc40fa7f83f035b648b5a555612195215
Michael Ball <cycomachead@gmail.com>
2019-10-13 14:03:45 -0700

    Update SSL Staging and deploy script

--- bin/deploy.sh
+++ bin/deploy.sh
@@ -1,22 +1,22 @@
 #! /usr/bin/env bash
 
 # Deploy a copy of snapCloud
-cd ~/snapCloud/
+pushd ~/snapCloud/
 git checkout master
 git pull origin master
 git submodule update --recursive --remote
 
 # Build community site
-cd site
+pushd site
 Snippets/build.sh
-cd ..
+popd
 
 # Create a release on GitHub
 source .env
 deploy_sha=$(git rev-parse HEAD)
 # A modified ISO8601 format. GitHub does not support tags
 current_time=$(date -u +"%Y-%m-%dT%H-%M-%S")
-repo='https://api.github.com/repos/bromagosa/snapCloud/releases'
+repo='https://api.github.com/repos/snap-cloud/snapCloud/releases'
 # tag non-production releases as 'prerelase'
 prelease=$(if [ "$LAPIS_ENVIRONMENT" = 'production' ]; then echo 'false'; else echo 'true'; fi)
 curl -X POST -H "Content-Type:application/json" -u cycomachead:$GITHUB_TOKEN $repo -d "{\"tag_name\": \"$current_time\", \"target_commitish\": \"$deploy_sha\", \"prerelease\": $prelease }"
@@ -27,3 +27,5 @@ curl -POST --url https://api.rollbar.com/api/1/deploy/ \
 
 # The cloud user only has the ability to restart this service.
 sudo service snapcloud_daemon restart
+
+popd
