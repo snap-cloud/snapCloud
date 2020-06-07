a30c5a5dce580a66374b0d24011afc89ddbaf0a2
Michael Ball <cycomachead@gmail.com>
2020-06-07 01:49:38 -0700

    Fix small bug in posting POSTs to Rollbar

--- resty/rollbar.lua
+++ resty/rollbar.lua
@@ -184,7 +184,13 @@ function _M.report(level, title)
   if method == 'GET' then
     request.GET = ngx.req.get_uri_args()
   elseif method == 'POST' then
-    request.POST = ngx.req.get_post_args()
+    ngx.req.read_body()
+    local args, err = ngx.req.get_post_args()
+    if err then
+      request.POST = 'ERROR READING POST ARGS'
+    else
+      request.POST = args
+    end
   end
 
   -- create a light thread to send the HTTP request in background
