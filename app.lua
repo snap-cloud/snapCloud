084b436da57b8e81f9096eb218483e1c62fb04d1
Michael Ball <cycomachead@gmail.com>
2020-06-24 01:54:05 -0700

    Undo temp maintenance test

--- app.lua
+++ app.lua
@@ -165,7 +165,7 @@ end
 
 -- Enable the ability to have a maintenance mode
 -- No routes are served, and a generic error is returned.
-if true or config.maintenance_mode == 'true' then
+if config.maintenance_mode == 'true' then
     local msg = 'The Snap!Cloud is currently down for maintenance.'
     app:match('/*', function(self)
         return errorResponse(msg, 500)
