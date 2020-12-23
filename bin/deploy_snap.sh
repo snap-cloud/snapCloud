0e27093ff7e389d80ca8887865c8281e3e3eef1f
Michael Ball <cycomachead@gmail.com>
2020-12-22 18:32:54 -0800

    Improve the Snap! deploy script

--- bin/deploy_snap.sh
+++ bin/deploy_snap.sh
@@ -1,32 +1,39 @@
 #! /usr/bin/env bash
 
-pushd /home/snapCloud/snap/
+base=/home/snapCloud/snap/
 
+pushd $base/snap/;
 echo "Updating Main Snap! Release"
 echo
-pushd snap
 git fetch origin
 snap_release=$(git describe --tags $(git rev-list --tags --max-count=1))
 echo "Checking out Snap! ${snap_release}"
 git checkout $snap_release
-popd
+popd;
+echo
+echo
 
+
+pushd $base/snap-versions/dev;
 echo "Updating DEVELOPMENT Snap!"
 echo
-pushd snap-versions/dev
 git fetch origin
 snap_release="origin/master"
 echo "Checking out Snap! $snap_release"
 git checkout origin/master
 popd;
+echo
+echo
 
 snap_release="v6.0.0" # must be exactly a git tag.
+pushd $base/snap-versions/previous;
 echo "Updating Previous Snap!: $snap_release"
 echo
-pushd snap-versions/previous
 git fetch origin
 echo "Checking out $snap_release"
 git checkout $snap_release
 popd;
+echo
+echo
 
 popd;
