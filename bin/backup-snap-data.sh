2364b71bfd77b8f10a35fe3ff7a0236e678b5668
Michael Ball <cycomachead@gmail.com>
2021-06-23 19:42:09 -0700

    Tweak backup script, still needs work.

--- bin/backup-snap-data.sh
+++ bin/backup-snap-data.sh
@@ -5,8 +5,12 @@ curl -XPOST https://hooks.slack.com/workflows/T02BLN36L/A01PUMAEUPR/344588914394
  -H 'Content-Type: application/json' \
  -d "{\"message\":\"Backup script started. $(date)\"}"
 
-server='backup@cloud.snap.berkeley.edu'
+server='cloud@cloud.snap.berkeley.edu'
 cloud_src="$server:/mnt/snap_cloud_project_storage/"
 local_dest='/volume1/snapcloud'
 
-rsync -az -e "ssh -p 22" --backup --backup-dir="rsync_bak_`date '+%F_%H-%M'`" /var/www backup@[nas-ip]:/volume1/Backups/
+rsync -az -e "ssh -p 22" --backup --backup-dir="rsync_bak_`date '+%F_%H-%M'`" $cloud_src $local_dest
+
+curl -XPOST https://hooks.slack.com/workflows/T02BLN36L/A01PUMAEUPR/344588914394149899/$SLACK_KEY \
+ -H 'Content-Type: application/json' \
+ -d "{\"message\":\"Backup script completed. $(date)\"}"
