0d387eea839eed48af240179a54b5ebf92eb82a2
Bernat Romagosa <bromagosa@e-citilab.eu>
2021-11-10 10:28:08 +0100

    added and formatted dates

--- app.lua
+++ app.lua
@@ -54,6 +54,16 @@ local domain_allowed = require('cors')
 -- Utility functions
 local date = require("date")
 
+string.from_sql_date = function (sql_date)
+    -- Formats an SQL date into (i.e.) November 21, 2021
+    month_names = { 'January', 'February', 'March', 'April', 'May', 'June',
+        'July', 'August', 'September', 'October', 'November', 'December' }
+    if (sql_date == nil) then return 'never' end
+    actual_date = date(sql_date)
+    return month_names[actual_date:getmonth()] ..  ' ' ..
+        actual_date:getday() ..  ', ' ..  actual_date:getyear()
+end
+
 -- wrap the lapis capture errors to provide our own custom error handling
 -- just do: yield_error({msg = 'oh no', status = 401})
 local lapis_capture_errors = package.loaded.app_helpers.capture_errors
