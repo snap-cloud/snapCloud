0726150bbe385ec087e9d8a88b7b821dd5db1af9
Bernat Romagosa <bernat@romagosa.work>
2022-08-02 22:11:07 +0200

    oops, missing EN locale entry

--- locales/en.lua
+++ locales/en.lua
@@ -161,6 +161,7 @@ local locale = {
     -- Search results page
     -- ===================
     search_results = "Search Results: @1",
+    project_search_results = "Projects matching: @1",
     collection_search_results = "Collections matching: @1",
     user_search_results = "Users Matching: @1",
     projects = "Projects",
