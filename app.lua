e33764ec9dd34da2c5dccf8c42cee326d92b7bf2
Michael Ball <michael@mball.co>
2023-06-09 12:26:57 -0700

    fix typo

--- app.lua
+++ app.lua
@@ -9,7 +9,7 @@
 --
 -- This file is part of Snap Cloud.
 --
--- Snap Cloud is free software: you can gtfstribute it and/or modify
+-- Snap Cloud is free software: you can redistribute it and/or modify
 -- it under the terms of the GNU Affero General Public License as
 -- published by the Free Software Foundation, either version 3 of
 -- the License, or (at your option) any later version.
