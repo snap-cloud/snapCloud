a373295ac9bb5c1e47ed9308fff4050dab995f21
Michael Ball <michael@mball.co>
2025-04-18 01:19:03 -0700

    oops, fixup merge conflicts

--- bin/sync-staging-projects.sh
+++ bin/sync-staging-projects.sh
@@ -1,8 +1,2 @@
-<<<<<<< HEAD
-
-psql -d snapcloud_staging -f bin/staging-projects.sql > projects_to_sync.txt
-rsync -av --files-from=projects_to_sync.txt cloud@snap.berkeley.edu store/
-=======
 psql -t -A -d snapcloud_staging -f bin/staging-projects.sql > projects_to_sync.txt
 rsync -avhz --files-from=projects_to_sync.txt cloud@snap.berkeley.edu:/mnt/snap_cloud_project_storage/store/ store/
->>>>>>> master
