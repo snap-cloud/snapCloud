25ff7b8686a66673a26f76f9bdb8bd1f7e2c49fd
Michael Ball <michael@mball.co>
2025-04-17 22:56:57 -0700

    Update the staging project syncing scripts

--- bin/staging-projects.sql
+++ bin/staging-projects.sql
@@ -50,7 +50,7 @@ all_projects AS (
 )
 
 -- SELECT COUNT(*) FROM all_projects;
--- SELECT (id / 1000) || '/' || id as project_path, *
-SELECT '/mnt/snap_cloud_project_storage/store/' || (id / 1000) || '/' || id as project_path
+SELECT (id / 1000) || '/' || id as project_path --, *
+-- SELECT '/mnt/snap_cloud_project_storage/store/' || (id / 1000) || '/' || id as project_path
 FROM all_projects
 ORDER BY id ASC;
