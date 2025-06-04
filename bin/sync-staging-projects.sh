f27001426117f626fcb38f0231f42a92463d9813
Michael Ball <michael@mball.co>
2025-06-04 00:37:51 -0700

    remove references to cloud.snap.berkeley.edu

--- bin/sync-staging-projects.sh
+++ bin/sync-staging-projects.sh
@@ -1,2 +1,2 @@
 psql -t -A -d snapcloud_staging -f bin/staging-projects.sql > projects_to_sync.txt
-rsync -avhz --files-from=projects_to_sync.txt cloud@snap.berkeley.edu:/mnt/snap_cloud_project_storage/store/ store/
+rsync -avhz --files-from=projects_to_sync.txt snap.berkeley.edu:/mnt/snap_cloud_project_storage/store/ store/
