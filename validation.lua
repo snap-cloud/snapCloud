5aeea5cabc4ddd0c863c9b3cf7f84ef51cdd7fba
Michael Ball <michael@mball.co>
2025-06-24 01:06:52 -0700

    Add a require models call.
    on some requests package.loaded.[model] is not actually loaded and leads to an error...
    it seems that expliciting (re?)loading models in the validations file prevents the error.

--- validation.lua
+++ validation.lua
@@ -20,6 +20,8 @@
 -- You should have received a copy of the GNU Affero General Public License
 -- along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
+require('models')
+
 local yield_error = package.loaded.yield_error
 local db = package.loaded.db
 local Collections = package.loaded.Collections
