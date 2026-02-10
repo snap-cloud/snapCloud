a79cccb299de4d22f7ff47ca71aa977ace0a167a
Bernat Romagosa <bromagosa@e-citilab.eu>
2026-02-10 11:50:29 +0100

    updated prereqs for latest openresty install steps

--- bin/prereqs.sh
+++ bin/prereqs.sh
@@ -33,8 +33,9 @@ if [ $? -ne 0 ]; then error; fi
 
 print_ok "Installing OpenResty..."
 apt-get -y install --no-install-recommends wget gnupg ca-certificates
-wget -O - https://openresty.org/package/pubkey.gpg | apt-key add -
-echo "deb http://openresty.org/package/ubuntu $(lsb_release -sc) main" \
+wget -O - https://openresty.org/package/pubkey.gpg | gpg --dearmor -o /etc/apt/trusted.gpg.d/openresty.gpg
+codename=`grep -Po 'VERSION="[0-9]+ \(\K[^)]+' /etc/os-release`
+echo "deb http://openresty.org/package/debian $codename openresty" \
     | tee /etc/apt/sources.list.d/openresty.list
 apt-get update
 apt-get -y install openresty
