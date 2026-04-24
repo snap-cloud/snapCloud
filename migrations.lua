68ffdd7d5d8036843d4e8c7fbb0526fa69e08ef0
Michael Ball <michael@mball.co>
2026-04-24 00:32:11 -0700

    ensure migration keys are unique, and update gitignore

--- migrations.lua
+++ migrations.lua
@@ -358,18 +358,6 @@ return {
         )
     end,
 
-    -- Add last_session_at column to users
-    -- Refactor session_count to track distinct sessions (4-hour window)
-    -- rather than login events
-    ['2026-04-06:0'] = function ()
-        schema.add_column(
-            'users',
-            'last_session_at',
-            types.time({ timezone = true, null = true })
-        )
-        update_user_views()
-    end,
-
   -- Add password_changed_at and updated_at columns to users
     ['2026-04-06:0'] = function ()
         schema.add_column(
@@ -380,6 +368,18 @@ return {
         schema.add_column(
             'users',
             'updated_at',
+            types.time({ timezone = true, null = true })
+        )
+        update_user_views()
+    end,
+
+    -- Add last_session_at column to users
+    -- Refactor session_count to track distinct sessions (4-hour window)
+    -- rather than login events
+    ['2026-04-06:2'] = function ()
+        schema.add_column(
+            'users',
+            'last_session_at',
             types.time({ timezone = true, null = true })
         )
         update_user_views()
